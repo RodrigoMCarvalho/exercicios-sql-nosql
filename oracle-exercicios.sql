65be9a2a26ec151b5c42605a59fed501d80557fb
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2019-03-17 21:58:32 -0300

    realizando exercicios

--- oracle-exercicios.sql
+++ oracle-exercicios.sql
@@ -11,6 +11,7 @@ CREATE SEQUENCE ID_SEQ;
 show parameter nls_lang;
 
 select * from compras;
+desc compras;
 
 select valor, observacoes from compras  
 where data >= '15-11-2008';
@@ -31,5 +32,75 @@ where id = 41;
 update compras
 set valor = 90.00
 where id = 41;
+
+alter table compras
+MODIFY ( observacoes varchar2(30) not null);
+
+
+alter table compras
+add (forma_pagt varchar2(15) check (forma_pagt in('CARTAO', 'DINHEIRO', 'BOLETO')));
+
+alter table compras
+rename column forma_pagt to forma_pagto;
+
+UPDATE compras
+set forma_pagto = 'CARTAO'
+where valor >= 900;
+
+update compras
+set forma_pagto = 'BOLETO'
+where valor BETWEEN 300 and 900;
+
+update compras
+set forma_pagto = 'DINHEIRO'
+where valor < 300;
+
+select sum(valor)as total from compras
+where forma_pagto = 'BOLETO';
+
+select forma_pagto, sum(valor) as total from compras
+group by forma_pagto;
+
+select forma_pagto, sum(valor) as total from compras
+group by forma_pagto
+HAVING sum(valor) > 5000;
+
+-- adicionar coluna ---
+alter table compras
+add (comprador_id number);
+
+alter table compras
+add foreign key (comprador_id) 
+references compradores(id);
+
+update compras
+set comprador_id = 1
+where id >= 20;
+
+update compras
+set comprador_id = 2
+where id <20;
+
+create table compradores (
+id number primary key,
+nome varchar2(30) not null,
+endereco varchar2(30) not null,
+telefone varchar2(20) not null);
+
+create sequence id_compradores_seq;
+
+insert into compradores (id, nome, endereco, telefone) 
+values (id_compradores_seq.nextval, 'Rodrigo' , 'Rua A', '9999-9999');
+
+insert into compradores (id, nome, endereco, telefone) 
+values (id_compradores_seq.nextval, 'Gustavo' , 'Rua A', '9889-9229');
+
+select * from compradores;
+
+select observacoes, valor, nome from compras
+join compradores
+on compras.comprador_id = compradores.id;
+
+
 
 
